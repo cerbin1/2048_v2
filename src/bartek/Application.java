93cd967f23cca4df2d48ec726cef100f577279e1
Bartek <bartek9393@poczta.onet.pl>
2017-04-27 22:41:55 +0100

    Added JFrame

--- src/bartek/Application.java
+++ src/bartek/Application.java
@@ -1,7 +1,18 @@
 package bartek;
 
-public class Application {
-    public static void main(String[] args) {
+import javax.swing.*;
 
+public class Application {
+    public void displayFrame() {
+        JFrame frame = new JFrame("1024");
+        frame.setSize(200, 200);
+        frame.setLocationRelativeTo(null);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Application().displayFrame();
     }
 }
