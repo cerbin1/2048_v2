baea8c5c40ae6755482bf661c1ab782323220f31
Bartek <bartek9393@poczta.onet.pl>
2017-05-01 20:59:07 +0100

    Added class Row

---
+++ src/bartek/Row.java
@@ -0,0 +1,47 @@
+package bartek;
+
+public class Row {
+    private final int[] row;
+
+    public Row(int one, int two, int three, int four) {
+        this.row = new int[]{one, two, three, four};
+    }
+
+    public int[] joinAndMove() {
+        for (int i = 0; i < 3; i++) {
+            if (row[i] == 0) {
+                moveEmptyFieldsOnEnd(i);
+            } else if (row[i + 1] == 0) {
+                moveEmptyFieldsOnEnd(i + 1);
+            }
+            if (canJoinFields(i)) {
+                joinWithNext(i);
+            }
+        }
+        return row;
+    }
+
+    private void moveEmptyFieldsOnEnd(int startingField) {
+        int fieldsLeft = startingField;
+        while (fieldsLeft < 3 && row[startingField] == 0) {
+            collapseFrom(startingField);
+            fieldsLeft++;
+        }
+    }
+
+    private boolean canJoinFields(int i) {
+        return row[i] == row[i + 1];
+    }
+
+    private void collapseFrom(int startingPosition) {
+        for (int j = startingPosition; j < 3; j++) {
+            row[j] = row[j + 1];
+        }
+        row[3] = 0;
+    }
+
+    private void joinWithNext(int i) {
+        row[i] += row[i + 1];
+        row[i + 1] = 0;
+    }
+}
---
+++ test/bartek/RowTest.java
@@ -0,0 +1,275 @@
+package bartek;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class RowTest {
+    @Test
+    public void shouldMoveField1() {
+        // given
+        Row row = new Row(2, 0, 0, 0);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{2, 0, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldMoveField2() {
+        // given
+        Row row = new Row(0, 2, 0, 0);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{2, 0, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldMoveField3() {
+        // given
+        Row row = new Row(0, 0, 2, 0);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{2, 0, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldMoveField4() {
+        // given
+        Row row = new Row(0, 0, 0, 2);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{2, 0, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldJoinTwoFields1() {
+        // given
+        Row row = new Row(2, 0, 0, 2);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{4, 0, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldJoinTwoFields2() {
+        // given
+        Row row = new Row(2, 0, 2, 0);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{4, 0, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldJoinFourFields() {
+        // given
+        Row row = new Row(2, 2, 8, 8);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{4, 16, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldJoinAndStopOnField() {
+        // given
+        Row row = new Row(0, 4, 2, 2);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{4, 4, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldMoveFields() {
+        // given
+        Row row = new Row(0, 2, 0, 4);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{2, 4, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldNotMoveAndJoin() {
+        // given
+        Row row = new Row(2, 4, 8, 32);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{2, 4, 8, 32},
+                after);
+    }
+
+    @Test
+    public void shouldJoinOnlyTwoFields() {
+        // given
+        Row row = new Row(2, 2, 4, 0);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{4, 4, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldPreferJoinFieldsOnLeft1() {
+        // given
+        Row row = new Row(2, 2, 2, 0);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{4, 2, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldPreferJoinFieldsOnLeft2() {
+        // given
+        Row row = new Row(2, 2, 0, 2);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{4, 2, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldPreferJoinFieldsOnLeft3() {
+        // given
+        Row row = new Row(0, 2, 2, 2);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{4, 2, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldPreferJoinFieldsOnLeft4() {
+        // given
+        Row row = new Row(2, 0, 2, 2);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{4, 2, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldDoNothingOnEmptyFields() {
+        // given
+        Row row = new Row(0, 0, 0, 0);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{0, 0, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldJoinFourFieldsAndNotJoinMore() {
+        // given
+        Row row = new Row(2, 2, 2, 2);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{4, 4, 0, 0},
+                after);
+    }
+
+    @Test
+    public void shouldMoveLastField() {
+        // given
+        Row row = new Row(2, 4, 0, 8);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{2, 4, 8, 0},
+                after);
+    }
+
+    @Test
+    public void shouldMoveLastFieldWhenTwoCentralAreEmpty() {
+        // given
+        Row row = new Row(4, 0, 0, 8);
+
+        // when
+        int[] after = row.joinAndMove();
+
+        // then
+        System.out.println(Arrays.toString(after));
+        assertArrayEquals(new int[]{4, 8, 0, 0},
+                after);
+    }
+}
