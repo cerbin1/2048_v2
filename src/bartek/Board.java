eedc08fb99ca2d0a41c33a3abc6a11a07f47fae1
Bartek <bartek9393@poczta.onet.pl>
2017-05-03 17:17:35 +0100

    Removed redundant arrays values initialization

--- src/bartek/Board.java
+++ src/bartek/Board.java
@@ -1,15 +1,10 @@
 package bartek;
 
 public class Board {
-    int[][] fields;
+    private final int[][] fields;
 
     public Board() {
         fields = new int[4][4];
-        for (int y = 0; y < 4; y++) {
-            for (int x = 0; x < 4; x++) {
-                fields[x][y] = 0;
-            }
-        }
     }
 
     public void setValue(int x, int y, int value) {
@@ -48,7 +43,6 @@ public class Board {
                 fields[x][y] = array[x];
             }
         }
-
     }
 
     public void moveFieldsRight() {
