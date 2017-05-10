25142eebc227d6f9c32630392019711e84051a65
Bartek <bartek9393@poczta.onet.pl>
2017-05-10 18:00:06 +0100

    Fixed merge

--- src/bartek/logic/ArrayHelper.java
+++ src/bartek/logic/ArrayHelper.java
@@ -1,10 +1,8 @@
 package bartek.logic;
 
-final class ArrayHelper {
-    static int[] revertValues(int[] array) {
-        int[] reverted = new int[array.length];
 public final class ArrayHelper {
     public static int[] revertValues(int[] array) {
+        int[] reverted = new int[array.length];
         for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
             reverted[i] = array[array.length - i - 1];
